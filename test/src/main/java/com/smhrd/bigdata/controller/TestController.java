cf8c69f745aa9fde446d2bdaae4c7d12b7a5beab
leejooooooon <dlwns7293@naver.com>
2023-10-16 11:26:20 +0900

    test

---
+++ test/src/main/java/com/smhrd/bigdata/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.smhrd.bigdata.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+	@GetMapping("/")
+	public String boardList(Model model) {
+		return "account-billing";
+	}
+}
