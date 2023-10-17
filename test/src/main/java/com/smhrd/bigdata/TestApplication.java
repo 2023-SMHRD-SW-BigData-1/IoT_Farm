b2e4616be2821e9ad8e09de13ea3eaf8165c5c54
usernamekimdaeun <daeun1505@naver.com>
2023-10-17 10:45:49 +0900

    123

--- test/src/main/java/com/smhrd/bigdata/TestApplication.java
+++ test/src/main/java/com/smhrd/bigdata/TestApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestApplication {
 	int a = 3;
+	int b=2;
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
 	}
