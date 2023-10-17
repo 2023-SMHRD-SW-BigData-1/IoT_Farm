071e66f5cd150f27b683a17957dc01a7464cb3bf
leejooooooon <dlwns7293@naver.com>
2023-10-17 10:34:49 +0900

    int a = 3;

--- test/src/main/java/com/smhrd/bigdata/TestApplication.java
+++ test/src/main/java/com/smhrd/bigdata/TestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestApplication {
-
+	int a = 3;
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
 	}
